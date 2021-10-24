ed2ed62291e97bc2837dae7c2eb8dca593feaf23
Park Chan ho <cksgh4178@naver.com>
2021-10-24 15:46:02 +0900

    update code

---
+++ Gradient-Based Learning Applied to Document Recognition/src/LeNet_5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 15:17:14 2021
+
+@author: Park Chan Ho
+"""
+
+import torch
+import torch.nn as nn
+
+class LeNet5(nn.Module):
+    
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        
+        return
